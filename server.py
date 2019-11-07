c222ac1f9f4fa9e38b48dd89679c172332fe1d28
Apostol <neketapostolov@gmail.com>
2019-11-07 22:31:15 +0200

    kek

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
 import socket
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 serv_socket.bind(('127.0.0.1', 53210))
-serv_socket.listen()
+serv_socket.listen(10)
+while True:
+    client_sock, client_address = serv_socket.accept()
+    print("Connected by", client_address)
+    while True:
+        data = client_sock.recv(1024)
+        print("POLUCHENO", data)
+        if not data:
+            break
+        client_sock.sendall(b"KEK")
+    client_sock.close()
