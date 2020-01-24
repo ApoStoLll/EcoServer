2a39afffcac039d291e9f2a2e6e95ed609493ebd
Apostol <neketapostolov@gmail.com>
2020-01-24 18:43:49 +0200

    httpserver

---
+++ first.py
@@ -0,0 +1,9 @@
+import server as sv
+host = '192.168.0.135'
+port = 8080
+name = 'kek'
+serv = sv.LolHTTPServer(host, port, name)
+try:
+    serv.serve_forever()
+except KeyboardInterrupt:
+    pass
--- server.py
+++ server.py
@@ -1,70 +1,132 @@
 import socket
+import sys
 import threading
+from email.parser import Parser
+from functools import lru_cache
+from urllib.parse import parse_qs, urlparse
 
-def run_server(port=8080):
-    server_socket = create_server_socket(port)
-    client_id = 0
-    while True:
-        client_socket = accept_client_connection(server_socket, client_id)
-        t = threading.Thread(target=serve_client, args=(client_socket, client_id))
-        t.start()
-        client_id += 1
-        #serve_client(client_socket, client_id)
+MAX_LINE = 64*1024
+MAX_HEADERS = 100
 
-def serve_client(client_socket, client_id):
-    request = read_request(client_socket)
-    if request is None:
-        print(f'Client #{client_id} unexpectedly disconnected')
-    else:
-        response = handle_request(request)
-        write_response(client_socket, response, client_id)
+class LolHTTPServer:
+    def __init__(self, host, port, server_name):
+        self._host = host
+        self._port = port
+        self._server_name = server_name
+    
+    def serve_forever(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+        try:
+            server_socket.bind((self._host, self._port))
+            server_socket.listen()
+            while True:
+                conn, _ = server_socket.accept()
+                try:
+                    #self.serve_client(conn)
+                    t = threading.Thread(target=self.serve_client, args=(conn))
+                    t.start()
+                except Exception as e:
+                    print('Client serving failed', e)
+        finally:
+            server_socket.close()
+    
+    def serve_client(self, conn):
+        try:
+            req = self.parse_request(conn)
+            resp = self.handle_request(req)
+            self.send_response(conn, resp)
+        except ConnectionResetError:
+            conn = None
+        except Exception as e:
+            self.send_error(conn, e)
+        if conn:
+            conn.close()
 
-def read_request(client_socket):
-    request = bytearray()
-    try:
+    def parse_request(self, conn):
+        rfile = conn.makefile('rb')
+        method, target, ver = self.parse_request_line(rfile)
+        headers = self.parse_headers(rfile) 
+        host = headers.get('Host')
+        if not host:
+            raise Exception("Bad request")
+        if host not in (self._server_name, f'{self._server_name}:{self._port}'):
+            raise Exception('Not found')
+        return Request(method, target, ver, headers, rfile)
+
+    def parse_request_line(self, rfile):
+        raw = rfile.readline(MAX_LINE + 1)
+        if len(raw) > MAX_LINE:
+            raise Exception('Request line is too long')
+        request_line = str(raw, 'iso-8859-1')
+        request_line = request_line.rstrip('\r\n')
+        words = request_line.split()
+        if len(words) != 3:
+            raise Exception('Malformed request line')
+        #method, target, ver = words
+        if words[2] != 'HTTP/1.1':
+            raise Exception('Unexpected HTTP version')
+        return words
+
+    def parse_headers(self, rfile):
+        headers = []
         while True:
-            chunk = client_socket.recv(4)
-            if not chunk: 
-                return None #Client unexpectedly disconnected
-            request += chunk
-            return request
-    except ConnectionResetError:
-        return None #Pizda
-    except:
-        raise #генерирует исключение
+            line = rfile.readline(MAX_LINE + 1)
+            if len(line) > MAX_LINE:
+                raise Exception('Header line is too long')
+            if line in (b'\r\n', b'\n', b''):
+                break
+            headers.append(line)
+            if len(headers) > MAX_HEADERS:
+                raise Exception('Too many headers')
+        sheaders = b''.join(headers).decode('iso-8859-1')
+        return Parser().parsestr(sheaders)
 
-def handle_request(request):
-    print(request)
-    return request
-    #obrabotka
+    def handle_request(self, req):
+        #if req.path == '/users' and req.method == 'POST'
+        return Response(200, 'OK')
 
-def write_response(client_socket, response, client_id):
-    client_socket.sendall(response)
-    client_socket.close()
-    print(f'Client #{client_id} has been served')
+    def send_response(self, conn, resp):
+        wfile = conn.makefile('wb')
+        status_line = f'HTTP/1.1 {resp.status} {resp.reason}\r\n'
+        wfile.write(status_line.encode('iso-8859-1'))
+        if resp.headers:
+            for (key, value) in resp.headers:
+                header_line = f'{key}: {value}\r\n'
+                wfile.write(header_line.encode('iso-8859-1'))
+        wfile.write(b'\r\n')
+        if resp.body:
+            wfile.write(resp.body)
+        wfile.flush()
+        wfile.close()
 
-def create_server_socket(server_port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    server_socket.bind(('192.168.0.135', server_port))
-    server_socket.listen()
-    return server_socket
+    def send_error(self, conn, err):
+        return 0 #do smth
 
-def accept_client_connection(serv_socket, client_id):
-    client_socket, client_address = serv_socket.accept()
-    print(f'Client #{client_id} connected')
-    return client_socket
+class Request:
+    def __init__(self, method, target, version, headers, rfile):
+        self.method = method
+        self.target = target
+        self.version = version
+        self.headers = headers
+        self.rfile = rfile
 
-run_server()
-#serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-#serv_socket.bind(('192.168.0.135', 8080))
-#serv_socket.listen(10)
-#while True:
-#    client_sock, client_address = serv_socket.accept()
-#    print("Connected by", client_address)
-#    while True:
-#        data = client_sock.recv(1024)
-#        print("POLUCHENO", data)
-#        if not data:
-#            break
-#        client_sock.sendall(b"KEK")
-#    client_sock.close()
+    @property
+    def path(self):
+        return self.url.path
+
+    @property
+    @lru_cache(maxsize=None)
+    def query(self):
+        return parse_qs(self.url.query)
+
+    @property
+    @lru_cache(maxsize=None)
+    def url(self):
+        return urlparse(self.target)
+
+class Response:
+  def __init__(self, status, reason, headers=None, body=None):
+    self.status = status
+    self.reason = reason
+    self.headers = headers
+    self.body = body
